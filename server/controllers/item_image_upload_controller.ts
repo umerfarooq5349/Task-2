73d707d7291efa239998fc60151d523601d82aff
Umer Farooq <umer.farooq@dicetech.dev>
2024-05-24 09:56:02 +0500

    gitignore updated

--- server/controllers/item_image_upload_controller.ts
+++ server/controllers/item_image_upload_controller.ts
@@ -2,5 +2,8 @@ import { Request, Response } from "express";
 import multer from "multer";
 
 export const uploadImage = (req: Request, res: Response) => {
-  res.json({ status: "Item uploaded successfully" });
+  res.status(200).json({
+    status: "Item uploaded successfully",
+    data: req.file,
+  });
 };
--- server/routes/item_image_upload.ts
+++ server/routes/item_image_upload.ts
@@ -1,7 +1,22 @@
 import Express from "express";
 import { uploadImage } from "./../controllers/item_image_upload_controller";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const uploadStorage = multer({ storage });
+
 const item_image_upload_router = Express.Router();
 
-item_image_upload_router.route("/").post(uploadImage);
+item_image_upload_router
+  .route("/")
+  .post(uploadStorage.single("avatar"), uploadImage);
 
 export default item_image_upload_router;
